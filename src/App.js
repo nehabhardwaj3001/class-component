f9852d6cb801d54de65e887821f2f1731eafa34e
neha@1309 <nehabhardwaj3001>
2022-06-15 10:38:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+// constructor(props) {
+//   super(props);
+//   this.state = {latitude : null, errorMessage : ''};
+// }
+
+state = {latitude : null, errorMessage : ''};
+
+componentDidMount() {
+  console.log("component mounted")
+  window.navigator.geolocation.getCurrentPosition(
+    Position =>{
+      this.setState( { latitude : Position.coords.latitude})
+    }, 
+    error => {
+      this.setState( { errorMessage : error.message})
+    }
+  )
 }
 
-export default App;
+componentDidUpdate() {
+  console.log("updated component")
+}
+
+  render() {
+   if(!this.state.errorMessage && this.state.latitude){
+      return <SeasonDisplay lat = {this.state.latitude}/>;
+   }
+    else if(this.state.errorMessage && !this.state.latitude) {
+     return ( <div> error: {this.state.errorMessage} </div>);
+     
+    }else {
+      return <Spinner message="Please accept location request" /> 
+    }
+  }
+}
+
+export default (App);
